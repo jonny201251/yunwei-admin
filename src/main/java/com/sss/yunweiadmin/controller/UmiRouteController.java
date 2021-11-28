a4a2787232a27e20f8fa3357c6ade2726d0d4a98
jonny201251 <jonny201251@163.com>
2021-11-28 13:26:49 +0800

    a

--- src/main/java/com/sss/yunweiadmin/controller/UmiRouteController.java
+++ src/main/java/com/sss/yunweiadmin/controller/UmiRouteController.java
@@ -17,7 +17,6 @@ public class UmiRouteController {
 
     @GetMapping("/redirect")
     public String redirect() {
-        System.out.println("redirect");
         //
         return "redirect:/ssologin";
     }
